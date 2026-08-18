a2a6e8d09a845990ef95dcb864cd3e30d3c5f89f
akshat-gupta-111 <akshatguptamtr@gmail.com>
2026-08-18 17:29:15 +0530

    feat : defined tool definitions for telemetry_tools

---
+++ pixos/src/pixos/tools/telemetry_tools.py
@@ -0,0 +1,18 @@
+from langchain.tools import tool
+
+@tool
+def get_cloudwatch_metrics(instance_id : str) -> str:
+    return "{'cpu':80, 'memory':50}"
+
+
+@tool
+def get_application_logs(app_id : str) -> str:
+    return "Crash"
+
+@tool
+def get_recent_deployments() -> str:
+    return "Deployment : 1111"
+
+@tool
+def fetch_k8s_pod_logs() -> str:
+    return "logs - 1"
--- pixos/tests/error_schema_test.py
+++ pixos/tests/error_schema_test.py
@@ -9,12 +9,12 @@ def test_error_schema_strict_validation():
                 detail="test-detail",
                 recovery_hint="test-hint"
             )
-    # Wrap in pytest.raises because we EXPECT a validation failure
+    
     with pytest.raises(ValidationError):
-        # We call ErrorSchema(**dict) to actually trigger Pydantic validation
+        
         ErrorSchema(
             type="https//:example.com",
-            status="403",  # This will now trigger a ValidationError under strict=True
+            status="403",  
             detail="test-detail",
             recovery_hint="test-hint"
         )
