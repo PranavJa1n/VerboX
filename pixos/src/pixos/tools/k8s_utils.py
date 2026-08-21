a6962b473435e3035c52b565100b428caa47fcdb
Pranav Jain <pranav.avlok@gmail.com>
2026-08-21 15:36:10 +0530

    feat(k8s_utils): implement EKS cluster operations and mock networking setup. This change adds the core delete_cluster, describe_cluster, and list_all_clusters lifecycle functions to the Kubernetes utilities module. It also introduces a setup_local_networking helper utility that provisions a mock VPC configuration containing two dedicated subnets to support local integration testing.

--- pixos/src/pixos/tools/k8s_utils.py
+++ pixos/src/pixos/tools/k8s_utils.py
@@ -3,7 +3,7 @@ from botocore.exceptions import ClientError
 import time
 from pixos.state.state import ErrorSchema
 
-FLOCI_URL = "http://localhost:4566"
+FLOCI_URL : str = "http://localhost:4566"
 
 ec2_client = boto3.client(
     "ec2",
@@ -21,8 +21,19 @@ eks_client = boto3.client(
     aws_secret_access_key="mock-secret"
 )
 
+def setup_local_networking() -> list:
+    """ Creates a mock VPC and 2 Subnets """
+    vpc = ec2_client.create_vpc(CidrBlock="10.0.0.0/16")
+    vpc_id = vpc["Vpc"]["VpcId"]
+    sub1 = ec2_client.create_subnet(VpcId=vpc_id, CidrBlock="10.0.1.0/24", AvailabilityZone="us-east-1a")
+    subnet_1_id = sub1["Subnet"]["SubnetId"]
+    sub2 = ec2_client.create_subnet(VpcId=vpc_id, CidrBlock="10.0.2.0/24", AvailabilityZone="us-east-1b")
+    subnet_2_id = sub2["Subnet"]["SubnetId"]
+    return [subnet_1_id, subnet_2_id]
+
 # Cluster Managment
 def create_cluster(subnet_ids : list[str], cluster_name : str, k8s_version : str, role_arn : str,) -> str:
+    """ Creates an eks cluster """
     try:
         response = eks_client.create_cluster(
             name=cluster_name,
@@ -32,23 +43,66 @@ def create_cluster(subnet_ids : list[str], cluster_name : str, k8s_version : str
                 "subnetIds": subnet_ids,
                 },
             )
-        return f"Created a new kubernetes cluster named {cluster_name} of version {k8s_version}"
+        return f"Created a new kubernetes cluster named {cluster_name} of version {k8s_version}. Response - {response}"
     except:
         return ErrorSchema(
-            type="www.google.com",
-            status="500",
-            detail=f"Kubernetes cluster named {cluster_name} failed to be created",
-            recovery_hint="Enter a correct role arn",
+            type=r"www.google.com",
+            status=409,
+            detail=f"Kubernetes cluster named {cluster_name} already exist",
+            recovery_hint="",
         )
 
-def delete_cluster() -> str:
-    pass
+def delete_cluster(cluster_name : str,) -> str:
+    """ Deletes an eks cluster """
+    try:
+        nodegroup = eks_client.list_nodegroups(clusterName = cluster_name)['nodegroups']
+        for i in nodegroup:
+            eks_client.delete_cluster_nodegroup(clusterName = cluster_name, nodegroupName = i)
+        response = eks_client.delete_cluster(name= cluster_name)
+        return response
+    except:
+        return ErrorSchema(
+            type=r"www.google.com",
+            status=500,
+            detail=f"Kubernetes cluster named {cluster_name} might not exist",
+            recovery_hint="Enter the correct cluster name",
+        )
 
-def describe_cluster() -> str:
-    pass
+def describe_cluster(cluster_name : str) -> str:
+    """ Gives all the details about a cluster """
+    try:
+        response = eks_client.describe_cluster(name=cluster_name)
+        cluster_details : dict = response["cluster"]
+        return cluster_details
+    except:
+        return ErrorSchema(
+                    type=r"www.google.com",
+                    status=404,
+                    detail=f"Kubernetes cluster named {cluster_name} not found",
+                    recovery_hint="Enter the correct cluster name",
+                )
 
-def list_clusters() -> str:
-    pass
+def list_all_clusters() -> list:
+    """ List all the cluster that exist """
+    try:
+        response : dict = eks_client.list_clusters()
+        cluster_list : list = response['clusters']
+        if cluster_list:
+            return cluster_list
+        else:
+            return ErrorSchema(
+                type=r"www.google.com",
+                status=404,
+                detail="No cluster found",
+                recovery_hint="Create a cluster first",
+            )
+    except:
+        return ErrorSchema(
+            type=r"www.google.com",
+            status=500,
+            detail="Can't reach the server right now",
+            recovery_hint="Try again in some time",
+        )
 
 # Node group managment
 def create_node_group() -> str: # add more worker node
@@ -62,3 +116,16 @@ def describe_node_group() -> str:
 
 def list_node_group() -> str:
     pass
+
+if __name__ == '__main__':
+    subnet_ids = setup_local_networking()
+    res = create_cluster(subnet_ids=subnet_ids, cluster_name="testing", k8s_version="1.31", role_arn="arn:aws:iam::000000000000:role/fake-eks-role")
+    create_cluster(subnet_ids=subnet_ids, cluster_name="testing2", k8s_version="1.31", role_arn="arn:aws:iam::000000000000:role/fake-eks-role")
+    print(f"{res}\n\n")
+    details = describe_cluster(cluster_name="testing")
+    print(f"{details}\n\n")
+    cluster_list = list_all_clusters()
+    print(f"{cluster_list}\n\n")
+    deleted = delete_cluster(cluster_name="testing")
+    delete_cluster(cluster_name="testing2")
+    print(f"{deleted}")
